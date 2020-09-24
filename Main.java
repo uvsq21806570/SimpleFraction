dedd73aefe1a0974e2877897935deb42a1929341
Mattéo Giraudo <matteo.giraudo@ens.uvsq.fr>
2020-09-24 14:24:17 +0200

    Ajout d'un constructeur et d'une méthode toString dans la classe Fraction, ainsi qu'une vérification d'une fraction dans la classe Main à l'aide du mot-clé assert.

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println ("Hello World!");
+        System.out.println ("Hello World");
+        Fraction f = new Fraction (3,2);
+        assert f.toString() == "3/2";
     }
 }
